d104dff1b71a3b1f6a11b1f5fefb6a0ad7f2bde5
Avi Ramdial <aviramdial0@proton.me>
2025-11-15 02:58:42 -0500

    Removed reverse motors which should now work and also removed field centric for testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMecanum.java
@@ -23,8 +23,6 @@ public class TeleOpMecanum extends LinearOpMode {
         // If your robot moves backwards when commanded to go forwards,
         // reverse the left side instead.
         // See the note about this earlier on this page.
-        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
         // Retrieve the IMU from the hardware map
         IMU imu = hardwareMap.get(IMU.class, "imu");
@@ -41,45 +39,25 @@ public class TeleOpMecanum extends LinearOpMode {
 
         while (opModeIsActive()) {
             double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
-            double x = gamepad1.left_stick_x;
+            double x = gamepad1.left_stick_x*1.1;
             double rx = gamepad1.right_stick_x;
-            double brake = 1.0-gamepad1.right_trigger;
 
             // This button choice was made so that it is hard to hit on accident,
-            // it can be freely changed based on preference.
-            // The equivalent button is start on Xbox-style controllers.
-            if (gamepad1.options) {
-                imu.resetYaw();
-            }
-
-            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
-
-            // Rotate the movement direction counter to the bot's rotation
-            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
-            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
-
-            rotX = rotX * 1.1;  // Counteract imperfect strafing
+            // it can be freely changed based on preferenc
 
             // Denominator is the largest motor power (absolute value) or 1
             // This ensures all the powers maintain the same ratio,
             // but only if at least one is out of the range [-1, 1]
-            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
-            double frontLeftPower = (rotY + rotX + rx) / denominator;
-            double backLeftPower = (rotY - rotX + rx) / denominator;
-            double frontRightPower = (rotY - rotX - rx) / denominator;
-            double backRightPower = (rotY + rotX - rx) / denominator;
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
 
-            /* THE FOLLOWING IS PSEUDOCODE
-            if b is pressed on the gamepad {
-                while yaw/90 is not an int {
-                    turn to nearest 90 degrees
-                }
-            }
-            */
-            frontLeftMotor.setPower(frontLeftPower*brake);
-            backLeftMotor.setPower(backLeftPower*brake);
-            frontRightMotor.setPower(frontRightPower*brake);
-            backRightMotor.setPower(backRightPower*brake);
+            frontLeftMotor.setPower(frontLeftPower);
+            backLeftMotor.setPower(backLeftPower);
+            frontRightMotor.setPower(frontRightPower);
+            backRightMotor.setPower(backRightPower);
         }
     }
 }
