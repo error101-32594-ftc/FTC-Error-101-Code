cdc02a2699bafeb40270e2177afd23d8e3867bec
Avi Ramdial <aviramdial0@proton.me>
2025-11-19 20:51:29 -0500

    Added robot centric if some drivers prefer

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotCentricMecanum.java
@@ -0,0 +1,130 @@
+package org.firstinspires.ftc.teamcode;
+
+import androidx.annotation.NonNull;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.VoltageSensor;
+import java.io.IOException;
+
+@TeleOp
+public class RobotCentricMecanum extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Declare our motors
+        // Make sure your ID's match your configuration
+        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
+        DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
+        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
+        DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
+        DcMotor bigHooperMotor = hardwareMap.dcMotor.get("bigHooperMotor");
+        DcMotor smallHooperMotor = hardwareMap.dcMotor.get("smallHooperMotor");
+
+        VoltageSensor voltageSensor = hardwareMap.voltageSensor.iterator().next();
+
+        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+        // Reverse the right side motors. This may be wrong for your setup.
+        // If your robot moves backwards when commanded to go forwards,
+        // reverse the left side instead.
+        // See the note about this earlier on this page.
+
+        // Retrieve the IMU from the hardware map
+
+        DiagnosticLogger logger = getLogger();
+        Thread loggerRuntimeThread = new Thread(logger);
+        logger.resumeRun();
+        loggerRuntimeThread.start();
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x*1.1;
+            double rx = gamepad1.right_stick_x;
+            double brakePower = 1-gamepad1.right_trigger;
+            double bigHooperPower = -gamepad1.left_trigger;
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+            telemetry.addData("Right Stick X", rx);
+            telemetry.addData("Left Stick X", x);
+            telemetry.addData("Left Stick Y", y);
+            telemetry.addData("Right Trigger", gamepad1.right_trigger);
+            telemetry.addData("Left Trigger", gamepad1.left_trigger);
+            telemetry.addData("frontLeftMotor", frontLeftMotor.getPower());
+            telemetry.addData("frontRightMotor", frontRightMotor.getPower());
+            telemetry.addData("backLeftMotor", backLeftMotor.getPower());
+            telemetry.addData("backRightMotor", backRightMotor.getPower());
+            telemetry.addData("Voltage", voltageSensor.getVoltage());
+            telemetry.addData("A", gamepad1.a);
+            telemetry.addData("B", gamepad1.b);
+            telemetry.addData("X", gamepad1.x);
+            telemetry.addData("Y", gamepad1.y);
+            telemetry.update();
+
+            if (gamepad1.a && gamepad1.b && gamepad1.x && gamepad1.y)
+            {
+                logger.stopRun();
+            }
+            }
+
+            if (gamepad1.a)
+            {
+                smallHooperMotor.setPower(0.9);
+                bigHooperMotor.setPower(bigHooperPower);
+            } else
+            {
+                smallHooperMotor.setPower(0);
+                bigHooperMotor.setPower(0);
+            }
+
+            if (gamepad1.b)
+            {
+                bigHooperMotor.setPower(0);
+            }
+
+            frontLeftMotor.setPower(frontLeftPower*brakePower);
+            backLeftMotor.setPower(backLeftPower*brakePower);
+            frontRightMotor.setPower(frontRightPower*brakePower);
+            backRightMotor.setPower(backRightPower*brakePower);
+        }
+    }
+
+    @NonNull
+    private DiagnosticLogger getLogger()
+    {
+        DiagnosticLogger logger;
+        try
+        {
+            logger = new DiagnosticLogger(
+                    telemetry, hardwareMap,
+                    new String[]
+                            {
+                                    "frontLeftMotor", "backLeftMotor",
+                                    "frontRightMotor", "backRightMotor",
+                                    "bigHooperMotor", "smallHooperMotor"
+                            },
+                    null, null, "imu", parameters
+            );
+        } catch (IOException e)
+        {
+            telemetry.addData("IOException", e.getMessage());
+            throw new RuntimeException(e);
+        }
+        return logger;
+    }
+}
