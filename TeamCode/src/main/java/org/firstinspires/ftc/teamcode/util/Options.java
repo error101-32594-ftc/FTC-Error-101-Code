2e870d505189b08577faf5909cfd78ef6e4d733e
ElectraBytes04 <electra-dev.404@protonmail.com>
2026-01-12 17:49:39 -0500

    Options.java development branch.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Options.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Options.java
@@ -3,39 +3,92 @@ package org.firstinspires.ftc.teamcode.util;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 @Autonomous(group = "Management")
 public class Options extends LinearOpMode
 {
-    private final String[] prompts = {
-            "Begin?",
-            "What alliance are you playing for?",
-            "Where do you plan to start from? (North is where the goals are.)",
-            "How many drivers?",
-            "Preferred driving style?"
-    };
-
-    private String[] modes = {"", "", "", "", "", ""};
-    private final boolean[] inputs = {
-            gamepad1.a, gamepad1.b, gamepad1.x, gamepad1.y,
-            gamepad1.left_bumper, gamepad1.right_bumper
-    };
-
     @Override
     public void runOpMode() throws InterruptedException
     {
-        String currentPrompt = prompts[0];
+        // Just asking about alliance for now.
+        String prompt = "What alliance are you playing for?";
 
-        telemetry.addData("Prompt", currentPrompt);
-        telemetry.addData(" A", modes[0]);
-        telemetry.addData(" B", modes[1]);
-        telemetry.addData(" X", modes[2]);
-        telemetry.addData(" Y", modes[3]);
-        telemetry.addData("LB", modes[4]);
-        telemetry.addData("RB", modes[5]);
+        telemetry.addLine(prompt);
+        telemetry.addLine("For RED Alliance, press B.");
+        telemetry.addLine("For BLU Alliance, press X.");
+        telemetry.addLine("The screen will clear when the OpMode starts.");
+        telemetry.update();
 
-        for(String prompt : prompts)
+        waitForStart();
+
+        while(opModeIsActive())
         {
-            System.out.println(prompt);
+            boolean red = gamepad1.b;
+            boolean blu = gamepad1.x;
+            boolean lb = gamepad1.left_bumper;
+
+            String select = "";
+            if(red)
+            {
+                select = "RED";
+            } else if(blu)
+            {
+                select = "BLU";
+            }
+
+            telemetry.clear();
+
+            if(lb)
+            {
+                telemetry.addLine(select + " was selected.");
+                telemetry.addLine("");
+                telemetry.update();
+
+                // Writing file:
+                String filename = "/sdcard/FIRST/DiagnosticLogs/alliance.txt";
+                String alliance = select.equals("BLU") ? "1" : "0";
+                try
+                {
+                    AllianceWriter writer = new AllianceWriter(filename);
+                    writer.write(alliance);
+                } catch(IOException e)
+                {
+                    throw new RuntimeException(e);
+                }
+            } else
+            {
+                telemetry.addLine(select + " is currently selected.");
+                telemetry.addLine("Press LEFT_BUMPER to finalize selection");
+                telemetry.update();
+            }
+        }
+    }
+
+    private static class AllianceWriter
+    {
+        final FileWriter fileWriter;
+        private final BufferedWriter bufferedWriter;
+
+        public AllianceWriter(String filename) throws IOException
+        {
+            File tmp = new File(filename);
+            if(!tmp.exists())
+            {
+                tmp.getParentFile().mkdirs();
+            }
+            fileWriter = new FileWriter(filename, false);
+            bufferedWriter = new BufferedWriter(fileWriter);
+        }
+
+        public void write(String alliance) throws IOException
+        {
+            bufferedWriter.write(alliance);
+            bufferedWriter.flush();
+            bufferedWriter.close();
         }
     }
 }
