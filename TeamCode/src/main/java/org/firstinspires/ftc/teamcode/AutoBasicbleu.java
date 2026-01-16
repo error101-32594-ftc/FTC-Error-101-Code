23d164881a0ed5283d207f7cc25d6ba3958feb8c
Avi Ramdial <aviramdial0@gmail.com>
2026-01-16 17:47:24 -0500

    AutoBasicbleu

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBasicbleu.java
@@ -0,0 +1,74 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.teamcode.util.TeamConstants;
+
+@Autonomous
+public class AutoBasicbleu extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Declare motors
+        // Make sure your ID's match your configuration
+        final DcMotorEx[] base = TeamConstants.getDriveMotors(hardwareMap);
+        final DcMotorEx[] scoring = TeamConstants.getScoringMotors(hardwareMap);
+
+        waitForStart();
+
+        if(isStopRequested()) return;
+
+        base[0].setPower(0.5);
+        base[1].setPower(0.5);
+        base[2].setPower(0.5);
+        base[3].setPower(0.5);
+        sleep(2700);
+        /*base[0].setPower(0.5);
+        base[1].setPower(0.5);
+        base[2].setPower(0.5);
+        base[3].setPower(0.5); */
+        base[0].setPower(-0.25);
+        base[1].setPower(-0.25);
+        base[2].setPower(0.25);
+        base[3].setPower(0.25);
+        sleep(975);
+        base[0].setPower(0);
+        base[1].setPower(0);
+        base[2].setPower(0);
+        base[3].setPower(0);
+        scoring[0].setVelocity(1920);
+        sleep(5000);
+        scoring[1].setPower(1);
+        scoring[2].setPower(1);
+        sleep(250);
+        scoring[1].setPower(0);
+        scoring[2].setPower(0);
+        sleep(5500);
+        scoring[1].setPower(1);
+        scoring[2].setPower(1);
+        sleep(1500);
+        scoring[0].setPower(0);
+        scoring[1].setPower(0);
+        scoring[2].setPower(0);
+        base[0].setPower(0.25);
+        base[1].setPower(0.25);
+        base[2].setPower(0.25);
+        base[3].setPower(0.25);
+        sleep(1000);
+        base[0].setPower(0.25);
+        base[1].setPower(0.25);
+        base[2].setPower(-0.25);
+        base[3].setPower(-0.25);
+        sleep(3150);
+        base[0].setPower(-0.5);
+        base[1].setPower(0.5);
+        base[2].setPower(-0.5);
+        base[3].setPower(0.5);
+        sleep(1250);
+
+
+    }
+};
